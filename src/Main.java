7df6dba8951bf914a8c8eae235b48bfd02bb8695
Aglesia <kikiglesia@live.fr>
2016-05-17 09:07:53 +0200

    Définition de toutes les classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main{
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("Test");
 	}
 }
